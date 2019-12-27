e33016dd588a134d52eca97986d83d730b5a5e88
asyslinux <syslinux@protonmail.com>
2019-12-27 11:21:01 +0300

    Initial commit

---
+++ mutex.go
@@ -0,0 +1,70 @@
+package mmutex
+
+import (
+	"math/rand"
+	"sync"
+	"time"
+)
+
+type Mutex struct {
+	locks       map[interface{}]interface{}
+	m           *sync.Mutex
+	lockRetries  int
+	lockDelay    float64
+	stdtDelay   float64
+	lockFactor  float64
+	lockJitter  float64
+}
+
+func (m *Mutex) TryLock(key interface{}) (gotLock bool) {
+	for i := 0; i < m.lockRetries; i++ {
+		m.m.Lock()
+		if _, ok := m.locks[key]; ok { // if locked
+			m.m.Unlock()
+			time.Sleep(m.moff(i))
+		} else { // if unlock, lockit
+			m.locks[key] = struct{}{}
+			m.m.Unlock()
+			return true
+		}
+	}
+
+	return false
+}
+
+func (m *Mutex) Unlock(key interface{}) {
+	m.m.Lock()
+	delete(m.locks, key)
+	m.m.Unlock()
+}
+
+func (m *Mutex) moff(retries int) time.Duration {
+	if retries == 0 {
+		return time.Duration(m.stdtDelay) * time.Nanosecond
+	}
+	moff, max := m.stdtDelay, m.lockDelay
+	for moff < max && retries > 0 {
+		moff *= m.lockFactor
+		retries--
+	}
+	if moff > max {
+		moff = max
+	}
+	moff *= 1 + m.lockJitter*(rand.Float64()*2-1)
+	if moff < 0 {
+		return 0
+	}
+	return time.Duration(moff) * time.Nanosecond
+}
+
+func NewMMutex(lockRetries int, lockDelay, stdtDelay, lockFactor, lockJitter float64) *Mutex {
+	return &Mutex{
+		locks:         make(map[interface{}]interface{}),
+		m:             &sync.Mutex{},
+		lockRetries:   300,
+		lockDelay:     10000000,
+		stdtDelay:     10000,
+		lockFactor:    1.1,
+		lockJitter:    0.2,
+	}
+}
