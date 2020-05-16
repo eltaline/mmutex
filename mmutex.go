4ac51be7b784c891d072014ac8f45558faa542b0
asyslinux <syslinux@protonmail.com>
2020-05-16 11:47:18 +0300

    Add GetLock() function

--- mmutex.go
+++ mmutex.go
@@ -16,48 +16,75 @@ type Mutex struct {
 	lockJitter  float64
 }
 
-func (m *Mutex) TryLock(key interface{}) (gotLock bool) {
+func (m *Mutex) GetLock(key interface{}) bool {
+
+	m.m.Lock()
+
+	if _, ok := m.locks[key]; ok {
+		return true
+	} else {
+		return false
+	}
+
+	return false
+
+}
+
+func (m *Mutex) TryLock(key interface{}) bool {
+
 	for i := 0; i < m.lockRetries; i++ {
+
 		m.m.Lock()
-		if _, ok := m.locks[key]; ok { // if locked
+
+		if _, ok := m.locks[key]; ok {
 			m.m.Unlock()
 			time.Sleep(m.moff(i))
-		} else { // if unlock, lockit
+		} else {
 			m.locks[key] = struct{}{}
 			m.m.Unlock()
 			return true
 		}
+
 	}
 
 	return false
 }
 
 func (m *Mutex) Unlock(key interface{}) {
+
 	m.m.Lock()
 	delete(m.locks, key)
 	m.m.Unlock()
+
 }
 
 func (m *Mutex) moff(retries int) time.Duration {
+
 	if retries == 0 {
 		return time.Duration(m.stdtDelay) * time.Nanosecond
 	}
+
 	moff, max := m.stdtDelay, m.lockDelay
 	for moff < max && retries > 0 {
 		moff *= m.lockFactor
 		retries--
 	}
+
 	if moff > max {
 		moff = max
 	}
+
 	moff *= 1 + m.lockJitter*(rand.Float64()*2-1)
 	if moff < 0 {
 		return 0
 	}
+
 	return time.Duration(moff) * time.Nanosecond
+
 }
 
 func NewMMutex() *Mutex {
+
 	return &Mutex{
 		locks:         make(map[interface{}]interface{}),
 		m:             &sync.Mutex{},
@@ -67,4 +94,5 @@ func NewMMutex() *Mutex {
 		lockFactor:    1.1,
 		lockJitter:    0.2,
 	}
+
 }
